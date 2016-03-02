e970c6ec72d90393b0d853d6016363a422ee4779
Mark West <markw627@gmail.com>
2016-03-01 19:14:13 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function addNums() {
+  num1 = document.getElementById("num1").value;
+  num2 = document.getElementById("num2").value;
+  document.getElementById("result").innerHTML = (parseInt(num1) + parseInt(num2));
+}
+
+// –––––––––– Javascript for the calculator exercise ––––––––––
